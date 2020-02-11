2a6cb9424f9eb4b4e542dac0f4e77ba5884b18c5
namkimdoan <namkimdoan@gmail.com>
2020-02-11 16:10:04 -0600

    Extend B and modify Driver to show it

---
+++ A.java
@@ -0,0 +1,7 @@
+public class A extends B{
+    private int data;
+    
+    public A(int data){
+        this.data = data;
+    }
+}
--- Driver.java
+++ Driver.java
@@ -2,5 +2,7 @@ public calss Driver{
     public static void main(String[] args){
         B b = new B(4);
         System.out.println(b);
+        A a = new A(10);
+        System.out.println(a);
     }
 }
